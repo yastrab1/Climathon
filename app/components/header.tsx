0593fb9825f23c05e357a30aacf13344f37338ea
Teo Visnovsky <teo.visno@gmail.com>
2024-10-27 08:07:11 +0100

    hehehe

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,11 +5,10 @@ import dynamic from 'next/dynamic';
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center bg-primaryYellow text-white p-4 shadow-md w-full h-10">
+    <div className="flex justify-between items-center bg-primaryYellow text-white p-4 shadow-md w-full h-13">
       <div className="flex items-center space-x-2">
         {/* Logo Placeholder */}
-        <div className="w-10 h-10 bg-white rounded-full"></div>
-        <span className="font-bold text-xl">KOLO</span>
+        <img src="/images/kolo.png" alt="" className='h-14' />
       </div>
       <nav>
         <ul className="flex space-x-6 font-semibold">
